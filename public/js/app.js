29a7b46b68d8480c1747aa72b5abb6a39a3b6e19
Davy Peter BRAUN <hello@davybraun.com>
2022-06-05 21:41:36 +0200

    feat: update team section

--- public/js/app.js
+++ public/js/app.js
@@ -42472,9 +42472,9 @@ __webpack_require__.r(__webpack_exports__);
     var expose = _ref.expose;
     expose();
     var people = [{
-      name: 'Bruxa',
+      name: 'Alexis',
       role: 'Founder and Partnerships',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1502702394563604480/6lW47Jth_400x400.jpg',
+      imageUrl: '/images/SP_AlexisPhoto_2.jpg',
       bio: 'She is a child of Brazilian Indigenous, Portuguese and adopted Jewish ancestors. She founded Thirsty Thirsty in 2014, offering a natural wine focused wine club, events, and consulting. She has worked in the global food and wine industry since 2009, focusing on natural wine generative farming in thirteen countries. She holds a WSET Level 3 accreditation with Distinction.',
       twitterUrl: 'https://twitter.com/0xbruxa',
       twitterName: '@0xbruxa'
@@ -42488,22 +42488,22 @@ __webpack_require__.r(__webpack_exports__);
     }, {
       name: 'Kim',
       role: 'Community Ops lead and Marketing',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1491188957429829640/WtSl34Gr_400x400.jpg',
-      bio: 'Kim has a background in marketing and project management. In the past, she has specialized in building partnerships and creating marketing campaigns for various direct-to-consumer brands like Grove Collaborative, Quip and Verb Energy. She’s bullish on the possibilities that Web3 can offer, especially in terms of bringing financial freedom to traditionally underrepresented groups. In 2021, she completed SheFi’s 10-week crypto course which helped shape her understanding of how Web3 technology and tools can help elevate various industries. At Thirsty Thirsty, she leads our marketing and community operations efforts. She’s eager and excited about Thirsty Thirsty’s mission to recenter Ancestral wisdom through joyful wine and food experiences powered by the blockchain.',
+      imageUrl: '/images/kim.jpg',
+      bio: 'Community Ops lead and Marketing. Kim is an NFT operations and marketing manager. In the past, she has specialized in building partnerships and creating marketing campaigns for various direct-to-consumer brands. She’s bullish on the possibilities that web3 can offer, especially in terms of bringing financial freedom to underrepresented groups.',
       twitterUrl: 'https://twitter.com/chimmyken',
       twitterName: '@chimmyken'
     }, {
       name: 'Trian',
       role: 'Artist Relations, Financials, Special Projects',
-      imageUrl: 'https://pbs.twimg.com/profile_images/1490861342424809483/_JT8PJbK_400x400.jpg',
-      bio: 'Trian began in web3 as an NFT collector and curator and quickly decided to develop a career in web3. Focused on woman lead projects. She joined the Thirsty Thirsty team after securing a scholarship to Eth Denver through Blue DAO. In addition to working with Thirsty Thirsty, she is a NFT art and music curator. While developing her own community driven project.',
+      imageUrl: '/images/trian.jpg',
+      bio: 'Artist Relations, Financials, Special Projects. Trian is an NFT art creative and financials lead. She is committed to supporting women in web3. Her passion is bringing diverse voices to the NFT and web3 space authentically. It is her belief that the community driven ethos of web3 and NFT’s can have a positive impact on the world.',
       twitterUrl: 'https://twitter.com/trianlongsmitty',
       twitterName: '@trian.eth'
     }, {
       name: 'Davy',
       role: 'Dev and Tech Strategy',
       imageUrl: 'https://pbs.twimg.com/profile_images/1508460189502128128/ML7QXfW0_400x400.jpg',
-      bio: 'An independent consultant working as a strategist, sometimes UX designer, oftentimes developer and tech lead for the web — and as of late, web3. An academically-trained designer turned technologist, he has worked extensively in the past 12 years for the luxury industry, art market, and institutions such as the OECD. He currently advises and leads tech for companies of different sizes and co-organized the first successful auction-house led NFT art sale in France, where he is based.',
+      bio: 'An independent consultant working as a strategist, designer and developer and tech lead for the web. An academically-trained designer turned technologist, he has worked extensively in the past 12 years for the luxury industry, art market, and institutions such as the OECD. He currently advises and leads tech for companies of different sizes and co-organized the first successful auction-house led NFT art sale in France, where he is based.',
       twitterUrl: 'https://twitter.com/davypeterbraun',
       twitterName: '@davypeterbraun'
     }];
